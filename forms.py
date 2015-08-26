9b037847039e9c638f8dae2d3f422857f600a923
Luke Maxwell <lukemaxwellshouse@gmail.com>
2015-08-26 15:56:46 +0100

    setup.py and project structure

--- forms.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form, StringField, PasswordField, validators, HiddenField
-
-class LoginForm(Form):
-    url = StringField('url', [validators.Required()])
-    username = StringField('username', [validators.Required()])
-    password = PasswordField('password', [validators.Required()])
-
-
