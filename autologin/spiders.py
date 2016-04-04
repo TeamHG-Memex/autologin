34ce30cdc5d65ec6791a6ac26268c4d53fe3b572
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-04-04 19:23:32 +0300

    Error handling: try to return status if possible

--- autologin/spiders.py
+++ autologin/spiders.py
@@ -71,27 +71,30 @@ class DefaultExecuteSplashRequest(SplashRequest):
     for the execute endpoint with support for POST requests and cookies.
     '''
     SPLASH_SCRIPT = '''
-    function last_response_headers(splash)
-        local entries = splash:history()
-        local last_entry = entries[#entries]
-        return last_entry.response.headers
-    end
-
     function main(splash)
         splash:init_cookies(splash.args.cookies)
-        assert(splash:go{
+        local ok, reason = splash:go{
             splash.args.url,
             headers=splash.args.headers,
             http_method=splash.args.http_method,
             body=splash.args.body,
-            })
-        assert(splash:wait(0.5))
-
-        return {
-            headers=last_response_headers(splash),
-            cookies=splash:get_cookies(),
-            html=splash:html(),
         }
+        if ok then
+            assert(splash:wait(0.5))
+        end
+
+        local entries = splash:history()
+        if #entries > 0 then
+            local last_response = entries[#entries].response
+            return {
+                headers=last_response.headers,
+                cookies=splash:get_cookies(),
+                html=splash:html(),
+                http_status=last_response.status,
+            }
+        else
+            assert(false, reason)
+        end
     end
     '''
 
